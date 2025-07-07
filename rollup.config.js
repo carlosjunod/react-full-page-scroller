70fc3b0432928d3335ee65c61632637c0bc2d686
CarlosJunod <carlos@junod.cl>
2025-07-07 00:14:36 -0500

    feat: update rollup configuration to support module exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
 import resolve from '@rollup/plugin-node-resolve';
-import babel   from '@rollup/plugin-babel';
+import babel from '@rollup/plugin-babel';
 
 export default {
+  type: "module",
+  main: "./dist/index.cjs.js",
+  module: "./dist/index.esm.js",
+  exports: {
+    require: "./dist/index.cjs.js",
+    import: "./dist/index.esm.js"
+  },
   input: 'src/index.jsx',
   output: [
     {
